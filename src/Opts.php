999c14b25caea63709adb4444827550a102ebdc1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-29 15:55:52 +0200

    Remove unused imports

--- src/Opts.php
+++ src/Opts.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Duon\Cli;
 
-use PhpParser\NodeVisitor\FirstFindingVisitor;
 use ValueError;
 
 /**
--- src/Runner.php
+++ src/Runner.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Duon\Cli;
 
-use BadMethodCallException;
 use Throwable;
 use ValueError;
 
